866d2b52230392aa16f624aac3ca5f81d42577cb
mdsaniulbasirsaz <saniulsaj@gmail.com>
2024-11-28 19:30:19 +0600

    add edit and delete function with fetch api

--- todolist/src/main/java/com/saniulsaz/todolist/Controller/TodoController.java
+++ todolist/src/main/java/com/saniulsaz/todolist/Controller/TodoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,15 +39,15 @@ public class TodoController {
         return todoRepository.findById(id).orElse(null);
     }
 
-    // // Update a ToDo
-    // @PutMapping("/{id}")
-    // public Todo updateTodo(@PathVariable String id, @RequestBody Todo updatedTodo) {
-    //     return todoRepository.findById(id).map(todo -> {
-    //         todo.setTitle(updatedTodo.getTitle());
-    //         todo.setCompleted(updatedTodo.isCompleted());
-    //         return todoRepository.save(todo);
-    //     }).orElse(null);
-    // }
+    // Update a ToDo
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable String id, @RequestBody Todo updatedTodo) {
+        return todoRepository.findById(id).map(todo -> {
+            todo.setTitle(updatedTodo.getTitle());
+            todo.setCompleted(updatedTodo.isCompleted());
+            return todoRepository.save(todo);
+        }).orElse(null);
+    }
 
     // Delete a ToDo
     @DeleteMapping("/{id}")
--- todolist/src/main/java/com/saniulsaz/todolist/Model/Todo.java
+++ todolist/src/main/java/com/saniulsaz/todolist/Model/Todo.java
@@ -33,6 +33,10 @@ public class Todo {
         return title;   
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public boolean isCompleted() 
     {
         return completed;
